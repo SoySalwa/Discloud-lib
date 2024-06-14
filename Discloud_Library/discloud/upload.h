762dd27aaa18cc2d51337795753e9d086afb2bb8
SoySalwa <zoldyckk54@gmail.com>
2024-06-14 14:24:02 -0400

    You can now try to make a request to upload a file to the API.

--- Discloud_Library/discloud/upload.h
+++ Discloud_Library/discloud/upload.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef UPLOAD_H
 #define UPLOAD_H
 
@@ -12,12 +13,67 @@ namespace dcl {
         /**
          * Make a request to the DisCloud API to upload a file.
          * @param discloud_token The DisCloud API token.
-         * @param end_point The API endpoint to make the request to.
+         * @param end_point The API endpoint make the request to.
+         * @param file_path, it's the file to upload.
          * @return The API response as a string (JSON).
          */
-        std::string upload_file(const dcl::discloud& discloud_token, const std::string& end_point) {
-            dcl::discloud discloud(discloud_token);
-            return discloud.route(end_point, "POST");
+        void send_upload(const dcl::discloud& discloud_token, const std::string& end_point, const std::string file_path = "") {
+            try {
+                dcl::discloud discloud(discloud_token);
+                std::string _response = discloud.route(end_point, "POST", file_path);
+
+                if (!_response.empty()) {
+                    try {
+                        auto json_response = nlohmann::json::parse(_response);
+                        set_response(_response);
+                    }
+                    catch (const nlohmann::json::parse_error& e) {
+                        std::cerr << "JSON parse error: " << e.what() << std::endl;
+                    }
+                }
+                else {
+                    std::cerr << "The response is null or empty." << std::endl;
+                }
+            }
+            catch (const std::runtime_error& e) {
+                std::cerr << "An error occurred: " << e.what() << std::endl;
+            }
+            catch (const std::exception& e) {
+                std::cerr << "An unexpected error occurred: " << e.what() << std::endl;
+            }
+        }
+
+        const std::string get_message() {
+            try {
+                json response_data = json::parse(response);
+
+
+                std::string message = response_data["message"];
+                return message;
+            }
+            catch (...) {
+                return "";
+            }
+        };
+        /**
+        * Get the response Json received of Discloud API.
+        * @return The response json, the response already parsed.
+        */
+        std::string get_json() const {
+            try {
+                json response_data = json::parse(response);
+                return "Discloud Response: " + response_data.dump(4);
+            }
+            catch (const json::parse_error& e) {
+                throw std::runtime_error("JSON parse error: " + std::string(e.what()));
+            }
+        }
+    private:
+        std::string response;
+        std::string _response;
+
+        void set_response(const std::string& new_response) {
+            response = new_response;
         }
 	};
 }
